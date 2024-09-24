4e00b1a1b2650169476e746682384eddfa863fcb
Stacy <jlhclientsolutions@gmail.com>
2024-09-24 14:21:20 -0400

    added project list file and added it to App

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,86 @@ import Header from './components/Header';
 import List from './components/Project';
 import Footer from './components/Footer';
 
-export default function App({ Component, pageProps }) {
+const projects = [
+  {
+    id: 1,
+    title: "HTML SEO Refactor",
+    link: '',
+    github: ''
+  },
+  {
+    id: 2,
+    title: "",
+    link: '',
+    github: ''
+  },
+  {
+    id: 3,
+    title: "",
+    link: '',
+    github: ''
+  },
+  {
+    id: 4,
+    title: "",
+    link: '',
+    github: ''
+  },
+  {
+    id: 5,
+    title: "",
+    link: '',
+    github: ''
+  },
+  {
+    id: 6,
+    title: "",
+    link: '',
+    github: ''
+  },
+  {
+    id: 7,
+    title: "",
+    link: '',
+    github: ''
+  },
+  {
+    id: 8,
+    title: "",
+    link: '',
+    github: ''
+  },
+  {
+    id: 9,
+    title: "",
+    link: '',
+    github: ''
+  },
+  {
+    id: 10,
+    title: "",
+    link: '',
+    github: ''
+  },
+  {
+    id: 11,
+    title: "",
+    link: '',
+    github: ''
+  },
+  {
+    id: 12,
+    title: "",
+    link: '',
+    github: ''
+  },
+];
+
+function App() {
+  return <List projects={projects} />;
+}
+
+export default function App() {
     return (
       <>
         <Nav />
--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -3,13 +3,14 @@ function List(props) {
       <ul className="list-group">
         {/* Here we map over each grocery item and return a new array of `li` elements that contains the grocery name */}
         {/* When using map you must provide a unique key attribute to each item. Ours is `item.id` */}
-        {props.projects.map(item => (
-          <li className="list-group-item" key={item.id}>
-            {item.name}
+        {props.projects.map(project => (
+          <li className="list-group-item" key={project.id}>
+            {project.title} 
+            {project.link}
+            {project.github}
           </li>
         ))}
       </ul>
     );
   }
-  
   export default List;
