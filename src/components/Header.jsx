05adecabfd7506f30c11f304196efa119a9cf6a7
Stacy <jlhclientsolutions@gmail.com>
2024-09-24 13:06:42 -0400

    added Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import { useLocation } from 'react-router-dom';
 function Header() {
     const currentPage = useLocation().pathname;
   
-return (
+return currentPage (
     <header>
     <p>Stacy Herbert</p>
     </header>
